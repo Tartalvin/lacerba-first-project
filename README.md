42bcbc8c4b4e2288c7ddf10e705b0d5589619aee
Alvin <alvinljuljanovic02@gmail.com>
2022-05-18 12:20:29 +0200

    add Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# My First node project
+
+## this is the first node project for lacerba
+
+To run the project , use 'ts-node main.ts'
--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
 console.log("Hello World!")
+let myVar: number = 3
+myVar += 10
+
+interface myInterface {
+      myNumber: number;
+      myString: string;
+}
+
+let myInterface: myInterface = {
+   myNumber: 10,
+   myString: "Hello World!"
+};
+
